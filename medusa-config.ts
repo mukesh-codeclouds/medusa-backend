0f4c04230575b7e0d27ab60b9f0dac1787a295a2
Mukesh Goswami <dev@codeclouds.biz>
2025-07-02 16:30:38 +0530

    update for server

--- medusa-config.ts
+++ medusa-config.ts
@@ -11,12 +11,17 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    redisUrl: process.env.REDIS_URL,
   },
   modules: [
     {
       resolve: "./src/modules/brand",
     },
   ],
-
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+  },
 })
